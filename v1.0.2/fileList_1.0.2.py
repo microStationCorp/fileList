591efd336aaf17b917e205837d65b7050f1500b9
root <sujanmondal916@gmail.com>
2019-12-18 10:59:47 +0530

    'Select' Button added now you dont have to type full name of a folder or file, you can select the folder or file to go enter

--- v1.0.2/fileList_1.0.2.py
+++ v1.0.2/fileList_1.0.2.py
@@ -6,64 +6,80 @@ class fileList:
     __root = Tk()
     __filename = StringVar()
     __thisControlFrame = Frame(__root, borderwidth=2, relief=SUNKEN, pady=5, bg="#AAA")
-    __textArea = Listbox(__root)
-    __thisScrollBar = Scrollbar(__textArea)
+    __listArea = Listbox(__root)
+    __thisScrollBar = Scrollbar(__listArea)
 
     def __init__(self):
         self.__root.title("FileList")
         self.__root.geometry("400x300")
         self.__root.grid_rowconfigure(0, weight=1)
         self.__root.grid_columnconfigure(0, weight=1)
-        self.__textArea.grid(sticky=N + E + S + W, padx=10)
-        self.__textArea.insert(0, 'please type something')
+        self.__listArea.grid(sticky=N + E + S + W, padx=10)
+        self.__listArea.insert(0, ' please type something')
         self.__filename.trace("w", self.searchfile)
         self.__thisScrollBar.pack(side=RIGHT, fill=Y)
-        self.__thisScrollBar.config(command=self.__textArea.yview)
-        self.__textArea.config(yscrollcommand=self.__thisScrollBar.set)
+        self.__thisScrollBar.config(command=self.__listArea.yview)
+        self.__listArea.config(yscrollcommand=self.__thisScrollBar.set)
         self.__thisControlFrame.grid(sticky=N + E + S + W, row=1, column=0, padx=10, pady=10)
+        self.__listArea.config(font='monaco 11')
 
         self.__thisControlFrame.grid_columnconfigure(0, weight=1)
         self.__thisControlFrame.grid_rowconfigure(0, weight=1)
         Label(self.__thisControlFrame, text="Direction", bg="#AAA", font="monaco 12 bold").grid(pady=5)
-        fname = Entry(self.__thisControlFrame, textvariable=self.__filename, font='monaco')
-        fname.grid(sticky=E + W + N + S, padx=30, row=1, column=0)
-        fname.focus_set()
+        self.__fname = Entry(self.__thisControlFrame, textvariable=self.__filename, font='monaco')
+        self.__fname.grid(sticky=E + W + N + S, padx=30, row=1, column=0)
+        self.__fname.focus_set()
+        self.__buttonFrame = Frame(self.__thisControlFrame, borderwidth=2, relief=SUNKEN)
+        self.__buttonFrame.grid(row=2, column=0)
+        self.__buttonSelect = Button(self.__buttonFrame, text='Select', font='monaco 11', command=self.selectPath)
+        self.__buttonSelect.pack(side=LEFT)
+        # self.__buttonOpen = Button(self.__buttonFrame, text='Open', font='monaco 11',width=6)
+        # self.__buttonOpen.pack(side=LEFT)
+
+    def selectPath(self):
+        s = self.__listArea.get(ACTIVE)
+        sl = s.split(' ')
+        val = ''
+        for i in range(len(sl[1]) - len(self.__baseName)):
+            val = val + sl[1][i + len(self.__baseName)]
+        self.__fname.insert(END, f'{val}/')
+        self.__fname.focus_set()
 
     def run(self):
         self.__root.mainloop()
 
     def searchfile(self, *args):
-        self.__textArea.delete(0, END)
-        dirName = os.path.dirname(self.__filename.get())
-        baseName = os.path.basename(self.__filename.get())
+        self.__listArea.delete(0, END)
+        self.__dirName = os.path.dirname(self.__filename.get())
+        self.__baseName = os.path.basename(self.__filename.get())
 
         if '' == self.__filename.get():
             self.__root.title("FileList")
-            self.__textArea.insert(0, 'please type something')
+            self.__listArea.insert(0, ' please type something')
         elif '/' == self.__filename.get()[len(self.__filename.get()) - 1]:
             if 0 == self.directionCheck(self.__filename.get()):
-                self.__root.title(f'{dirName} - FileList')
+                self.__root.title(f'{self.__dirName} - FileList')
                 self.insertFileListInTextArea(self.__filename.get())
             elif 1 == self.directionCheck(self.__filename.get()):
-                self.__textArea.insert(0, f'{dirName} : not a directory')
+                self.__listArea.insert(0, f' {self.__dirName} : not a directory')
             elif 2 == self.directionCheck(self.__filename.get()):
-                self.__textArea.insert(0, f'{dirName} : file/folder not found')
+                self.__listArea.insert(0, f' {self.__dirName} : file/folder not found')
         else:
-            self.__root.title(f'{dirName} - FileList')
+            self.__root.title(f'{self.__dirName} - FileList')
             try:
-                listOfElement = os.listdir(dirName)
+                listOfElement = os.listdir(self.__dirName)
                 flag = False
                 for i in listOfElement:
-                    if i[0:len(baseName)] == baseName:
-                        if os.path.isfile(dirName + f"/{i}"):
-                            self.__textArea.insert(END, f"{i}  (file)")
+                    if i[0:len(self.__baseName)] == self.__baseName:
+                        if os.path.isfile(self.__dirName + f"/{i}"):
+                            self.__listArea.insert(END, f" {i}  (file)")
                         else:
-                            self.__textArea.insert(END, f"{i}  (folder)")
+                            self.__listArea.insert(END, f" {i}  (folder)")
                         flag = True
                 if not flag:
-                    self.__textArea.insert(0, 'File/Folder Not Found')
+                    self.__listArea.insert(0, ' File/Folder Not Found')
             except FileNotFoundError:
-                self.__textArea.insert(0, "Invalid Address")
+                self.__listArea.insert(0, " Invalid Address")
 
     @staticmethod
     def directionCheck(filename):
@@ -79,9 +95,9 @@ class fileList:
         __list = os.listdir(filename)
         for i in __list:
             if os.path.isfile(filename + f"/{i}"):
-                self.__textArea.insert(END, f"{i}  (file)")
+                self.__listArea.insert(END, f" {i}  (file)")
             else:
-                self.__textArea.insert(END, f"{i}  (folder)")
+                self.__listArea.insert(END, f" {i}  (folder)")
 
 
 List = fileList()
