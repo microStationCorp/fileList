a1ceffe0e08ff0cd7b977c78d744dceb23ee7af0
root <sujanmondal916@gmail.com>
2019-12-19 23:48:53 +0530

    minor bug fixes

--- v1.0.3/fileList_1.0.3.py
+++ v1.0.3/fileList_1.0.3.py
@@ -1,3 +1,4 @@
+import os
 from tkinter import *
 
 
@@ -35,7 +36,7 @@ class fileList:
         self.__fname.focus_set()
         self.__buttonFrame = Frame(self.__thisControlFrame, borderwidth=2, relief=SUNKEN)
         self.__buttonFrame.grid(row=2, column=0)
-        self.__buttonSelect = Button(self.__buttonFrame, text='Select', font='monaco 11')
+        self.__buttonSelect = Button(self.__buttonFrame, text='Select', font='monaco 11', command=self.selectPath)
         self.__buttonSelect.pack(side=LEFT)
         self.__buttonOpen = Button(self.__buttonFrame, text='Run', font='monaco 11', width=6)
         self.__buttonOpen.pack(side=LEFT)
@@ -43,7 +44,53 @@ class fileList:
     def run(self):
         self.__root.mainloop()
 
-    # def searchFile(self, *args):
+    def selectPath(self):
+        s = self.__listArea.get(ACTIVE)
+        if s.endswith('(file)') or s.endswith('(folder)'):
+            s = re.sub('^ ', '', s)
+            if s.endswith('(file)'):
+                s = re.sub(' {2}\(file\)$', '', s)
+            elif s.endswith('(folder)'):
+                s = re.sub(' {2}\(folder\)$', '', s)
+            val = ''
+            for i in range(len(s) - len(self.__baseName)):
+                val = val + s[i + len(self.__baseName)]
+            self.__fname.insert(END, f'{val}/')
+            self.__fname.focus_set()
+
+    def insertFileListInTextArea(self, filename):
+        try:
+            __list = os.listdir(filename)
+            for i in __list:
+                if os.path.isfile(filename + f"/{i}"):
+                    self.__listArea.insert(END, f" {i}  (file)")
+                else:
+                    self.__listArea.insert(END, f" {i}  (folder)")
+        except:
+            self.__listArea.insert(0, ' not a directory')
+
+    def searchFile(self, *args):
+        self.__listArea.delete(0, END)
+        self.__dirName, self.__baseName = os.path.split(self.__filename.get())
+        if self.__filename.get().endswith('/') and os.path.exists(self.__dirName):
+            self.insertFileListInTextArea(self.__dirName)
+            self.__root.title(f'{self.__dirName}-FileList')
+            if self.__listArea.size() == 0:
+                self.__listArea.insert(0, ' Folder is empty')
+        elif os.path.exists(self.__dirName):
+            self.__root.title(f'{self.__dirName}-FileList')
+            __list = os.listdir(self.__dirName)
+            for i in __list:
+                if i.startswith(self.__baseName) and os.path.isfile(os.path.join(self.__dirName, i)):
+                    self.__listArea.insert(END, f" {i}  (file)")
+                elif i.startswith(self.__baseName):
+                    self.__listArea.insert(END, f" {i}  (folder)")
+            if self.__listArea.size() == 0:
+                self.__listArea.insert(0, ' File or folder not found')
+        elif self.__filename.get() == '':
+            self.__listArea.insert(0, ' please type something')
+        else:
+            self.__listArea.insert(0, ' Invalid Address')
 
 
 List = fileList()
