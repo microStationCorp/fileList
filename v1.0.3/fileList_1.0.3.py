3bb2874a20bbf6027e81cd0f84268ec356d3462b
root <sujanmondal916@gmail.com>
2019-12-25 01:20:19 +0530

    deletion process added by pressing ctrl + d

--- v1.0.3/fileList_1.0.3.py
+++ v1.0.3/fileList_1.0.3.py
@@ -4,13 +4,25 @@ import sys
 from tkinter import *
 from tkinter.messagebox import *
 import re
+import shutil
+
+
+def validateName(name, ftype):
+    fileReg = re.compile('[~`$%^&*{}:;"\',<.>?/\[\]]')
+    folderReg = re.compile('[~`$%^&*{}:;"\',<>?/\[\]]')
+    if ftype == 'file' and fileReg.search(name) is None:
+        return True
+    elif ftype == 'folder' and folderReg.search(name) is None:
+        return True
+    else:
+        return False
 
 
 class fileList:
     __root = Tk()
     __filename = StringVar()
     __thisControlFrame = Frame(__root, borderwidth=2, relief=SUNKEN, pady=5, bg="#AAA")
-    __listArea = Listbox(__root)
+    __listArea = Listbox(__root, selectmode=EXTENDED)
     __thisScrollBarSide = Scrollbar(__listArea)
     __thisScrollBarBottom = Scrollbar(__listArea)
     __dirName = ''
@@ -19,7 +31,7 @@ class fileList:
 
     def __init__(self):
         self.__root.title("FileList")
-        x, y = self.sizeOfWindow(self.__root.winfo_screenwidth(), self.__root.winfo_screenheight(), 500, 500)
+        x, y = self.sizeOfWindow(500, 500)
         self.__root.geometry(f"500x500+{x}+{y}")
         self.__root.minsize(500, 500)
         self.__root.grid_rowconfigure(0, weight=1)
@@ -27,6 +39,7 @@ class fileList:
         self.__root.bind('<Control-n>', self.newFile)
         self.__root.bind('<Control-Shift-N>', self.newDir)
         self.__root.bind('<Control-h>', self.shortcutDialog)
+        self.__root.bind('<Control-d>', self.deleteContentSelected)
         self.__filename.trace('w', self.searchFile)
         self.__listArea.grid(sticky=N + E + S + W, padx=10)
         self.__listArea.insert(0, ' please type something')
@@ -43,7 +56,7 @@ class fileList:
 
         self.__thisControlFrame.grid_columnconfigure(0, weight=1)
         self.__thisControlFrame.grid_rowconfigure(0, weight=1)
-        Label(self.__thisControlFrame, text="Direction", bg="#AAA", font="monaco 12 bold").grid(pady=5)
+        Label(self.__thisControlFrame, text="Enter path Address :", bg="#AAA", font="monaco 12 bold").grid(pady=5)
         self.__fname = Entry(self.__thisControlFrame, textvariable=self.__filename, font='monaco')
         self.__fname.grid(sticky=E + W + N + S, padx=30, row=1, column=0)
         self.__fname.focus_set()
@@ -104,7 +117,7 @@ class fileList:
         self.__fname.focus_set()
 
     def dialogBox(self, title, cmd):
-        x, y = self.sizeOfWindow(self.__root.winfo_screenwidth(), self.__root.winfo_screenheight(), 300, 70)
+        x, y = self.sizeOfWindow(300, 70)
         self._rootTop = Toplevel()
         self._rootTop.title(title)
         self._rootTop.geometry(f'300x70+{x}+{y}')
@@ -135,7 +148,7 @@ class fileList:
     def createFile(self, *args):
         fileName = self.__topEntry.get()
         name, extension = os.path.splitext(fileName)
-        if name != '' and extension != '' and self.validateName(name,'file'==True):
+        if name != '' and extension != '' and validateName(name, 'file'):
             f = open(os.path.join(os.path.dirname(self.__filename.get()), fileName), 'w+')
             f.close()
             showinfo('info', f'{fileName} has created')
@@ -145,7 +158,7 @@ class fileList:
             showinfo('info', 'invalid name')
 
     def createDir(self, *args):
-        if self.__topEntry.get() != '' and self.validateName(self.__topEntry.get(),'folder')==True:
+        if self.__topEntry.get() != '' and validateName(self.__topEntry.get(), 'folder') == True:
             fileName = self.__topEntry.get()
             os.mkdir(os.path.join(os.path.dirname(self.__filename.get()), fileName))
             showinfo('info', f'{fileName} has created')
@@ -162,9 +175,12 @@ class fileList:
     ctrl + shift + N
 
 3. help : 
-    ctrl + H'''
+    ctrl + H
 
-        x, y = self.sizeOfWindow(self.__root.winfo_screenwidth(), self.__root.winfo_screenheight(), 300, 370)
+4. delete :
+    ctrl + D'''
+
+        x, y = self.sizeOfWindow(300, 370)
         _shortCutTop = Tk()
         _shortCutTop.title('shortcuts')
         _shortCutTop.geometry(f'300x370+{x}+{y}')
@@ -175,29 +191,38 @@ class fileList:
         _hLabel.pack(anchor='w')
         _content = Frame(_shortCutTop)
         _content.pack(side=TOP, fill=X, padx=10, pady=5)
-        _cLabel = Label(_content, text=text, font='monaco', justify=LEFT)
+        _cLabel = Label(_content, text=text, font='consolas 10', justify=LEFT)
         _cLabel.pack(anchor='nw')
         _statusBar = Frame(_shortCutTop, bg="#CCC")
         _statusBar.pack(side=BOTTOM, fill=X)
         _stLabel = Label(_statusBar, bg="#CCC", text='Created by Sherlock...', font='hack 7')
         _stLabel.pack(anchor='s')
 
-    def sizeOfWindow(self, rootWindowWidth, rootWindowHight, childWindowWidth, childWindowHight):
+    def sizeOfWindow(self, childWindowWidth, childWindowHight):
+        rootWindowWidth = self.__root.winfo_screenwidth()
+        rootWindowHeight = self.__root.winfo_screenheight()
+
         x = (rootWindowWidth / 2) - (childWindowWidth / 2)
-        y = (rootWindowHight / 2) - (childWindowHight / 2)
+        y = (rootWindowHeight / 2) - (childWindowHight / 2)
         return round(x), round(y)
 
-    def validateName(self,name,ftype):
-        fileReg=re.compile('[~`$%^&*{}:;"\'\\,<.>?/\[\]]')
-        folderReg=re.compile('[~`$%^&*{}:;"\'\\,<>?/\[\]]')
-        if ftype=='file' and fileReg.search(name)==None:
-            return True
-        elif ftype=='folder' and folderReg.search(name)==None:
-            return True
+    def deleteContentSelected(self, *args):
+        selectedIndex = self.__listArea.curselection()
+        if len(selectedIndex) != 0 and self.__filename.get() != '' and os.path.exists(
+                os.path.dirname(self.__filename.get())):
+            condition = askyesno('info', 'do you want to delete ?')
+            if condition:
+                for i in selectedIndex:
+                    if self.__listArea.get(i).endswith('(file)'):
+                        os.remove(os.path.join(os.path.dirname(self.__filename.get()), self.__listArea.get(i)[:-10]))
+                    elif self.__listArea.get(i).endswith('(folder)'):
+                        shutil.rmtree(
+                            os.path.join(os.path.dirname(self.__filename.get()), self.__listArea.get(i)[:-12]))
+                self.printList()
         else:
-            return False
-            
-        
+            showinfo('info', 'illegal action')
 
-List = fileList()
-List.run()
+
+if __name__ == '__main__':
+    List = fileList()
+    List.run()
